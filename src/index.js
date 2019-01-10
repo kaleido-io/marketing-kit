21ecbf82d7a208c60dd9683e10ce7c394699b730
Josh Mobley <joshua.mobley@consensys.net>
2019-01-10 13:54:09 -0500

    adding entrypoint for easier import

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import Alert from './Alert';
+import Banner from './Banner';
+import BannerShape from './BannerShape';
+import Button from './Button';
+import CallToAction from './CallToAction';
+import Footer from 'Footer';
+import Grid from 'Grid';
+import GridItem from 'GridItem';
+import Header from 'Header';
+import Infographic from 'InfoGgraphic';
+import Logo from 'Logo';
+import PieChart from 'PieChart';
+import Quote from 'Quote';
+import Section from 'Section';
+import Wrapper from 'Wrapper';
+
+export default {
+    Alert,
+    Banner,
+    BannerShape,
+    Button,
+    CallToAction,
+    Footer,
+    Grid,
+    GridItem,
+    Header,
+    Infographic,
+    Logo,
+    PieChart,
+    Quote,
+    Section,
+    Wrapper
+}
