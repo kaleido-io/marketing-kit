577390a4685d2ad1f2a22a9dae9ce3c4dc6105e9
Josh Mobley <joshua.mobley@consensys.net>
2019-01-10 11:43:59 -0500

    reorganizing storybook

---
+++ stories/components.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
+
+import Alert from '../src/Alert';
+import Button from '../src/Button';
+import Infographic from '../src/Infographic';
+import PieChart from '../src/PieChart';
+import Quote from '../src/Quote';
+
+storiesOf('Components', module)
+    .addDecorator(withInfo)
+    .addDecorator(withKnobs)
+    .add('Alert', () => 
+        <Alert 
+            message={text('message', 'This is my alert message.')}  
+            dismissText={text('dissmissText', 'Dismiss')} 
+            />
+    )
+    .add('Button', () => 
+        <Button 
+            text={ text('text', 'Button Text') } 
+            color={ select('color', ['purple', 'white', 'outline-purple', 'outline-white']) }
+            /> 
+    )
+    .add('Infographic', () => 
+        <Infographic 
+            fill={ number('fill', 100) } 
+            title={ text('title', 'Statistic') } 
+            text={ text('description', 'Description Text') } 
+            color={ select('color', ['green', 'orange', 'purple'], 'green')} 
+            />
+    )
+    .add('PieChart', () => 
+        <PieChart 
+            text={ text('text', undefined) }
+            />
+    )
+    .add('Quote', () => 
+        <Quote 
+            text={ text('text', 'This is the quote text.') }
+            headshot={ text('headshot', '//placehold.it/150x150') }
+            person={{
+                name: text('name', 'Jane Q. Public'),
+                title: text('title', 'CEO'),
+                org: text('org', 'Conglomo')
+            }}
+            isLarge={ boolean('isLarge', false) }
+            border={ boolean('border', false) }
+            noPadding={ boolean('noPadding', false) }
+            />
+    );
+
+
