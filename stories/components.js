8147143495737fea60a6e522ed5da1d80337bb7c
Josh Mobley <joshua.mobley@consensys.net>
2019-01-22 15:48:42 -0500

    tweaking card styles

--- stories/components.js
+++ stories/components.js
@@ -53,7 +53,7 @@ storiesOf('Components', module)
         <Grid columnCount="4">
             <GridItem>
                 <Card 
-                    image={ text('image', <img src="//placehold.it/150x150" /> )}
+                    image={ text('image', <img src="//placehold.it/75x75" /> )}
                     title={ text('text', 'Card Title') } 
                     text={ text('text', 'Card description text lorem ipsum...') }
                     meta={ <div>Meta</div> }
@@ -61,7 +61,7 @@ storiesOf('Components', module)
             </GridItem>
             <GridItem>
                 <Card 
-                    image={ text('image', <img src="//placehold.it/150x150" />) }
+                    image={ text('image', <img src="//placehold.it/75x75" />) }
                     title={ text('text', 'Card Title') } 
                     text={ text('text', 'Card description text lorem ipsum...') }
                     meta={ <div>Meta</div> }
@@ -69,7 +69,7 @@ storiesOf('Components', module)
             </GridItem>
             <GridItem>
                 <Card 
-                    image={ text('image', <img src="//placehold.it/150x150" />) }
+                    image={ text('image', <img src="//placehold.it/75x75" />) }
                     title={ text('text', 'Card Title') } 
                     text={ text('text', 'Card description text lorem ipsum...') }
                     meta={ <div>Meta</div> }
@@ -77,7 +77,7 @@ storiesOf('Components', module)
             </GridItem>
             <GridItem>
                 <Card 
-                    image={ text('image', <img src="//placehold.it/150x150" />) }
+                    image={ text('image', <img src="//placehold.it/75x75" />) }
                     title={ text('text', 'Card Title') } 
                     text={ text('text', 'Card description text lorem ipsum...') }
                     meta={ <div>Meta</div> }
