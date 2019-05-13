a48f870b4cdfaf39a61418a2d35f916a32a7159b
Josh Mobley <joshua.mobley@consensys.net>
2019-05-13 18:44:27 -0400

    updates for marketplace

--- dist/Logo/index.js
+++ dist/Logo/index.js
@@ -3,17 +3,22 @@ import React from 'react';
 import './Logo.scss';
 
 const Logo = ({
-  linkUrl = '/'
+  linkUrl = '/',
+  tagline
 }) => React.createElement("a", {
   className: "k-logo k-logo--stacked",
   href: linkUrl
 }, React.createElement("p", {
   className: "k-logo__title"
-}, "Kaleido"), React.createElement("p", {
+}, "Kaleido"), tagline && React.createElement("p", {
   className: "k-logo__tagline"
 }, "A ConsenSys Business"));
 
+Logo.defaultProps = {
+  tagline: false
+};
 Logo.propTypes = {
-  siteUrl: PropTypes.string
+  siteUrl: PropTypes.string,
+  tagline: PropTypes.bool
 };
 export default Logo;
